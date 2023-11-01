bb696349ab1453346f9fe95b3b4ec1d3e6e38f93
vinit-softdev <vinithpatel21@gnu.ac.in>
2023-11-02 00:38:50 +0530

    practical 11 done

---
+++ app/src/main/java/com/example/mad_practical11_21012011109/Person.kt
@@ -0,0 +1,27 @@
+package com.example.mad_practical11_21012011109
+
+import org.json.JSONObject
+import java.io.Serializable
+
+class Person(jsonObject: JSONObject): Serializable {
+
+    var Id:String
+    var Name:String
+    var EmailId:String
+    var PhoneNo:String
+    var Address:String
+    var Latitude:Double
+    var Longitude: Double
+
+    init {
+        Id = jsonObject.getString("id")
+        EmailId = jsonObject.getString("email")
+        PhoneNo = jsonObject.getString("phone")
+        val profileJson = jsonObject.getJSONObject("profile")
+        Name = profileJson.getString("name")
+        Address = profileJson.getString("address")
+        val locationJson = profileJson.getJSONObject("location")
+        Latitude = locationJson.getDouble("lat")
+        Longitude = locationJson.getDouble("long")
+    }
+}
